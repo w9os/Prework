81d142da89fc2b9469ba23247a42c05db3a894b6
w9os <79824189+w9os@users.noreply.github.com>
2021-03-24 22:37:28 -0400

    added files

---
+++ MODULE 6/javascript.js
@@ -0,0 +1,28 @@
+function Grow() {
+    document.getElementById('box').setAttribute('id', "grow")
+}
+    // document.getElementById("button1").
+//  $(document).ready()
+//  var document.getElementById("button1").addEventListener("click", function Blue( {
+//      document.getElementById("box").style.backgroundColor = "blue";
+//  });
+
+//  document.getElementById("button2").addEventListener("click", function Grow( {
+//      document.getElementById("box").style.height = "250px";
+//  });
+
+//  document.getElementById("button3").addEventListener("click", function Fade(){
+//      document.getElementById("box").style.opacity = "0.5";
+//  });
+
+//  document.getElementById("button4").addEventListener("click", function Reset(){
+//      document.getElementById("box").style.height = "150px";
+//  });
+
+//  document.getElementById("Reset").addEventListener("click", function Reset(){
+//      document.getElementById("box").style.opacity = "1";
+//  });
+
+//  document.getElementById("Reset").addEventListener("click", function Reset(){
+//      document.getElementById("box").style.backgroundColor = "orange";
+// });
